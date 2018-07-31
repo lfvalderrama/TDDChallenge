94fc58e66c74172fa24c72371ce988e98c773226
Luis Valderrama <lfvalderramav@unal.edu.co>
2018-07-31 14:04:30 -0500

    insertion sort solution

--- TDDChallenge/challenge.cs
+++ TDDChallenge/challenge.cs
@@ -6,8 +6,21 @@ namespace TDDChallenge
     {
         public int[] SortArray(int[] numbers)
         {
-            // Not developed yet.
-            throw new NotImplementedException();
+            int[] sortedArray = numbers;
+            for (int i = 0; i < sortedArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (sortedArray[j - 1] > sortedArray[j])
+                    {
+                        int temp = sortedArray[j - 1];
+                        sortedArray[j - 1] = sortedArray[j];
+                        sortedArray[j] = temp;
+                    }
+                }
+            }
+
+            return sortedArray;
         }
 
         public static void Main()
